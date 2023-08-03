cd7893b1444d238ca0f600ad89b526051beae99e
ThanePatrol <mandalidis.hugh@gmail.com>
2023-08-04 08:40:01 +1000

    added tmux config

--- home/tmux.nix
+++ home/tmux.nix
@@ -4,9 +4,19 @@
   home.packages = [ pkgs.tmux ];
 
   programs.tmux = {
+    terminal = "screen-256color";
     enable = true;
     mouse = true;
     shortcut = "a";
+    baseIndex = 1;
+    historyLimit = 5000;
+
+    plugins = [
+      pkgs.tmuxPlugins.sensible
+      pkgs.tmuxPlugins.catppuccin
+      pkgs.tmuxPlugins.yank
+
+    ];
   };
 
 
