ba04e15a13b81a3b4e8a2b8952ce734d6cdf0464
ThanePatrol <mandalidis.hugh@gmail.com>
2023-08-04 10:01:49 +1000

    added vim movements for tmux

--- home/tmux.nix
+++ home/tmux.nix
@@ -4,12 +4,13 @@
   home.packages = [ pkgs.tmux ];
 
   programs.tmux = {
-    terminal = "tmux-256color";
+    terminal = "xterm-256color";
     enable = true;
     mouse = true;
     shortcut = "a";
     baseIndex = 1;
     historyLimit = 5000;
+    keyMode = "vi";
 
     plugins = [
       pkgs.tmuxPlugins.sensible
@@ -19,6 +20,11 @@
     ];
     
     extraConfig = ''
+      bind h select-pane -L
+      bind j select-pane -D 
+      bind k select-pane -U
+      bind l select-pane -R
+
       set -g @catppuccin_no_patched_fonts_theme_enabled on
       set -g @catppuccin_date_time "%Y-%m-%d %H:%M"      
             
