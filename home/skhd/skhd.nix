1575fb944b8e73c8d921df773d8dcf7dfc539d0b
Thane-Patrol <hugh.man@hotmail.com>
2023-10-29 20:30:23 +1100

    changed skhd config to use alt instead of cmd

--- home/skhd/skhd.nix
+++ home/skhd/skhd.nix
@@ -3,28 +3,28 @@
 {
   home.file.".config/skhd/skhdrc".text = ''
     # change window focus
-    cmd - j : yabai -m window --focus south
-    cmd - k : yabai -m window --focus north
-    cmd - h : yabai -m window --focus west
-    cmd - l : yabai -m window --focus east
+    alt - j : yabai -m window --focus south
+    alt - k : yabai -m window --focus north
+    alt - h : yabai -m window --focus west
+    alt - l : yabai -m window --focus east
 
     # move windows around in a workspace
-    cmd + down : yabai -m window --swap south
-    cmd + up : yabai -m window --swap north
-    cmd + left : yabai -m window --swap west
-    cmd + right : yabai -m window --swap east
+    alt + down : yabai -m window --swap south
+    alt + up : yabai -m window --swap north
+    alt + left : yabai -m window --swap west
+    alt + right : yabai -m window --swap east
 
     # move around workspaces
-    cmd + shift - j : yabai -m space --focus south
-    cmd + shift - k : yabai -m space --focus north
-    cmd + shift - h : yabai -m space --focus west
+    alt + shift - j : yabai -m space --focus south
+    alt + shift - k : yabai -m space --focus north
+    alt + shift - h : yabai -m space --focus west
 
     # move window to another workspace
     # note that you can't follow without disabling SIP
-    cmd + shift + l : yabai -m window --space east
-    cmd + shift + h : yabai -m window --space west
-    cmd + shift + j : yabai -m window --space south
-    cmd + shift + k : yabai -m window --space north
+    alt + shift + l : yabai -m window --space east
+    alt + shift + h : yabai -m window --space west
+    alt + shift + j : yabai -m window --space south
+    alt + shift + k : yabai -m window --space north
 
     '';
 }
