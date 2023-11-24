0145d55d7936dea50ab175a58487eecdcca8a118
Hugh Mandalidis <hmandalidis@atlassian.com>
2023-11-24 20:24:00 +1100

    feat: unfree predicate for terraform

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let 
-  # TODO - make this a global that you only need to change once
+  # TODO - make this a global/param that you only need to change once
   # my work is different to my personal device...
   user_name = "hmandalidis";
 in {
@@ -111,41 +111,10 @@ in {
    # };
   };
 
-  # https://github.com/LnL7/nix-darwin/issues/214
-  # hack for getting nix apps to launch with spotlight
-  system.activationScripts.applications.text = lib.mkForce ''
-    echo "setting up ~/Applications..." >&2
-    applications="$HOME/Applications"
-    nix_apps="$applications/Nix Apps"
-
-    # Needs to be writable by the user so that home-manager can symlink into it
-    if ! test -d "$applications"; then
-        mkdir -p "$applications"
-        chown ${user_name}: "$applications"
-        chmod u+w "$applications"
-    fi
-
-    # Delete the directory to remove old links
-    rm -rf "$nix_apps"
-    mkdir -p "$nix_apps"
-    find ${config.system.build.applications}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
-            # Spotlight does not recognize symlinks, it will ignore directory we link to the applications folder.
-            # It does understand MacOS aliases though, a unique filesystem feature. Sadly they cannot be created
-            # from bash (as far as I know), so we use the oh-so-great Apple Script instead.
-            /usr/bin/osascript -e "
-                set fileToAlias to POSIX file \"$src\" 
-                set applicationsFolder to POSIX file \"$nix_apps\"
-                tell application \"Finder\"
-                    make alias file to fileToAlias at applicationsFolder
-                    # This renames the alias; 'mpv.app alias' -> 'mpv.app'
-                    set name of result to \"$(rev <<< "$src" | cut -d'/' -f1 | rev)\"
-                end tell
-            " 1>/dev/null
-        done
-  '';
-
-  users.users.${user_name} = { home = "/Users/${user_name}"; };
+  users.users.${user_name} = { 
+    name = user_name;
+    home = "/Users/${user_name}"; 
+  };
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
