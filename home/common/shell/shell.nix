7925f5348b394f4ea1eafe10d5066a033c3dc225
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2023-11-26 20:56:09 +1100

    :feat Added parameter attributes to linux config

--- home/common/shell/shell.nix
+++ home/common/shell/shell.nix
@@ -3,7 +3,7 @@ let
   linuxUpdate = ''
     sudo nix-channel --update && 
     sudo cp -r ~/nixos/* /etc/nixos &&
-    sudo nixos-rebuild switch &&
+    nixos-rebuild switch &&
     sudo nix-env --delete-generations 7d
   '';
   macUpdate = ''
