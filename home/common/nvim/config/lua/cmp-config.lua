6f4196c965d0358184558c3bdab02e5cd846cea3
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-01-01 20:31:56 +1100

    feat: improve suggestion hotkeys

--- home/common/nvim/config/lua/cmp-config.lua
+++ home/common/nvim/config/lua/cmp-config.lua
@@ -7,8 +7,8 @@ luasnip.config.setup {}
 cmp.setup {
     snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
     mapping = cmp.mapping.preset.insert {
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete {},
