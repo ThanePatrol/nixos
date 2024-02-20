025213969d3e20c95976d009962e01aaf7189a1b
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-02-20 21:36:23 +1100

    chore: open firewall

--- homelab/content/content-aggregation.nix
+++ homelab/content/content-aggregation.nix
@@ -4,14 +4,17 @@
 
   services.sonarr = {
     enable = true;
+    openFirewall = true;
   };
 
   services.radarr = {
     enable = true;
+    openFirewall = true;
   };
 
   services.transmission = {
     enable = true;
+    openFirewall = true;
     settings = {
       download-dir = "/nfs/samsung4tb/Content/Downloads";
       webHome = pkgs.flood;
