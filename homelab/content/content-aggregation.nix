3f23b5e899bdf5eab1589e3e193b02e7ef9378e3
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-02-20 21:05:59 +1100

    fix: remove data dirs

--- homelab/content/content-aggregation.nix
+++ homelab/content/content-aggregation.nix
@@ -4,17 +4,14 @@
 
   services.sonarr = {
     enable = true;
-    dataDir = "$HOME/.config";
   };
 
   services.radarr = {
     enable = true;
-    dataDir = "$HOME/.config";
   };
 
   services.transmission = {
     enable = true;
-    home = "$HOME/.config";
     settings = {
       download-dir = "/nfs/samsung4tb/Content/Downloads";
       webHome = pkgs.flood;
