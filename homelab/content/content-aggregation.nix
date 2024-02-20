3a5f0ecf2aed7b92f62764e362fbb3e2508342a6
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-02-20 21:08:06 +1100

    feat: added paths back

--- homelab/content/content-aggregation.nix
+++ homelab/content/content-aggregation.nix
@@ -1,17 +1,20 @@
 {pkgs, ...}:
-
+# FIXME - take inputs to read these
 {
 
   services.sonarr = {
     enable = true;
+    dataDir = "/home/hugh/.config";
   };
 
   services.radarr = {
     enable = true;
+    dataDir = "/home/hugh/.config";
   };
 
   services.transmission = {
     enable = true;
+    home = /home/hugh/.config;
     settings = {
       download-dir = "/nfs/samsung4tb/Content/Downloads";
       webHome = pkgs.flood;
