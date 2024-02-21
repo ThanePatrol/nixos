a574390a28ce9f8255f1f0566dca018968f5581c
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-02-22 09:48:37 +1100

    feat: added upload and download speed limits

--- homelab/configuration.nix
+++ homelab/configuration.nix
@@ -78,6 +78,7 @@ in
     vim
     zsh
     git
+    sabnzbd
   ];
 
   virtualisation.libvirtd = {
--- homelab/content/content-aggregation.nix
+++ homelab/content/content-aggregation.nix
@@ -32,6 +32,10 @@
       download-dir = "/nfs/samsung4tb/Content/Downloads";
       rpc-bind-address = "127.0.0.1,10.0.0.*";
       rpc-whitelist = "127.0.0.1,10.0.0.*";
+      speed-limit-up-enabled = true;
+      speed-limit-up = 300; #KBs per second
+      speed-limit-down-enabled = true;
+      speed-limit-down = 2000;
     };
     webHome = pkgs.flood-for-transmission;
   };
