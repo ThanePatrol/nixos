72352a507b0af44ca298c5e1a856e7004a2f0482
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-03-11 08:55:26 +1100

    feat: starship to p10k

--- home/common/shell/shell.nix
+++ home/common/shell/shell.nix
@@ -65,14 +65,13 @@ in {
     enableAutosuggestions = true;
     envExtra = ''
       export EDITOR="nvim"
-      export STARSHP_CONFIG="$HOME/.config/starship.toml"
       '' 
       + (if isDarwin then macExports else "");
 
     shellAliases = {
       ".." = "cd ..";
-      "..." = "cd ...";
-      "...." = "cd ....";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
       ll = "ls -l";
       ls = "ls --color=auto";
       open = (if isDarwin then "open" else "xdg-open");
@@ -93,127 +92,136 @@ in {
         name = "zsh-syntax-highlighting";
         src = pkgs.zsh-syntax-highlighting;
       }
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k-config;
+        file = "p10k.zsh";
+      }
     ];
     initExtra = ''
       eval "$(direnv hook zsh)"
-      eval "$(starship init zsh)"
       '' 
       + (if isDarwin then macInit else "")
       + (if isWork then workExports else "");
 
   };
 
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-    settings = {
-      add_newline = true;
+ # programs.starship = {
+ #   enable = true;
+ #   enableZshIntegration = true;
+ #   settings = {
+ #     add_newline = true;
 
-      character = {
-        success_symbol = "[❯](maroon)";
-        error_symbol = "[❯](red)";
-        vimcmd_symbol = "[❮](green)";
-      };
-      format = lib.concatStrings [ "$directory" "$line_break" "$character" ];
+ #     character = {
+ #       success_symbol = "[❯](maroon)";
+ #       error_symbol = "[❯](red)";
+ #       vimcmd_symbol = "[❮](green)";
+ #     };
+ #     format = lib.concatStrings [ "$directory" "$line_break" "$character" ];
 
-      git_branch = {
-        format = "[$branch]($style) ";
-        symbol = " ";
-      };
+ #     git_branch = {
+ #       format = "[$branch]($style) ";
+ #       symbol = " ";
+ #     };
 
-      git_status = {
-        format =
-          "[[($conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
-        conflicted = "≠";
-        untracked = "";
-        modified = "*";
-        staged = "";
-        renamed = "";
-        deleted = "";
-        stashed = "≡ ";
-        ahead = "⇡ ";
-      };
+ #     git_status = {
+ #       format =
+ #         "[[($conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+ #       conflicted = "≠";
+ #       untracked = "";
+ #       modified = "*";
+ #       staged = "";
+ #       renamed = "";
+ #       deleted = "";
+ #       stashed = "≡ ";
+ #       ahead = "⇡ ";
+ #     };
 
-      aws = {
-        symbol = "☁️ ";
-        format = "[$symbol$profile]($style) ";
-      };
+ #     aws = {
+ #       symbol = "☁️ ";
+ #       format = "[$symbol$profile]($style) ";
+ #     };
 
-      docker_context = { 
-        symbol = "🐋"; 
-      #  format = "[$symbol $context]($style) ";
-      };
+ #     docker_context = { 
+ #       symbol = "🐋"; 
+ #     #  format = "[$symbol $context]($style) ";
+ #     };
 
-      c = { symbol = "🔧"; };
-      helm = { symbol = "⎈ "; };
-      java = { symbol = "☕ "; };
-      lua = { symbol = "🌙 "; };
-      nix_shell = { symbol = "❄️ "; };
-      nodejs = { symbol = "⬢ "; };
-      python = { symbol = "🐍 "; };
-      rust = { symbol = "🦀 "; };
-      sudo = { symbol = "🧙 "; };
-      terraform = { symbol = "💠 "; };
-      right_format = " $all";
-      palette = starshipThemeString;
-      palettes.catppuccin_mocha = {
-        rosewater = "#f5e0dc";
-        flamingo = "#f2cdcd";
-        pink = "#f5c2e7"; # put semicolons on the end of each line
-        mauve = "#cba6f7";
-        red = "#f38ba8";
-        maroon = "#eba0ac";
-        peach = "#fab387";
-        yellow = "#f9e2af";
-        green = "#a6e3a1";
-        teal = "#94e2d5";
-        sky = "#89dceb";
-        sapphire = "#74c7ec";
-        blue = "#89b4fa";
-        lavender = "#b4befe";
-        text = "#cdd6f4";
-        subtext1 = "#bac2de";
-        subtext0 = "#a6adc8";
-        overlay2 = "#9399b2";
-        overlay1 = "#7f849c";
-        overlay0 = "#6c7086";
-        surface2 = "#585b70";
-        surface1 = "#45475a";
-        surface0 = "#313244";
-        base = "#1e1e2e";
-        mantle = "#181825";
-        crust = "#11111b";
-      };
-      palettes.catppuccin_latte = {
-        rosewater = "#dc8a78";
-        flamingo = "#dd7878";
-        pink = "#ea76cb";
-        mauve = "#8839ef";
-        red = "#d20f39";
-        maroon = "#e64553";
-        peach = "#fe640b";
-        yellow = "#df8e1d";
-        green = "#40a02b";
-        teal = "#179299";
-        sky = "#04a5e5";
-        sapphire = "#209fb5";
-        blue = "#1e66f5";
-        lavender = "#7287fd";
-        text = "#4c4f69";
-        subtext1 = "#5c5f77";
-        subtext0 = "#6c6f85";
-        overlay2 = "#7c7f93";
-        overlay1 = "#8c8fa1";
-        overlay0 = "#9ca0b0";
-        surface2 = "#acb0be";
-        surface1 = "#bcc0cc";
-        surface0 = "#ccd0da";
-        base = "#eff1f5";
-        mantle = "#e6e9ef";
-        crust = "#dce0e8";
-      };
-    };
-  };
+ #     c = { symbol = "🔧"; };
+ #     helm = { symbol = "⎈ "; };
+ #     java = { symbol = "☕ "; };
+ #     lua = { symbol = "🌙 "; };
+ #     nix_shell = { symbol = "❄️ "; };
+ #     nodejs = { symbol = "⬢ "; };
+ #     python = { symbol = "🐍 "; };
+ #     rust = { symbol = "🦀 "; };
+ #     sudo = { symbol = "🧙 "; };
+ #     terraform = { symbol = "💠 "; };
+ #     right_format = " $all";
+ #     palette = starshipThemeString;
+ #     palettes.catppuccin_mocha = {
+ #       rosewater = "#f5e0dc";
+ #       flamingo = "#f2cdcd";
+ #       pink = "#f5c2e7"; # put semicolons on the end of each line
+ #       mauve = "#cba6f7";
+ #       red = "#f38ba8";
+ #       maroon = "#eba0ac";
+ #       peach = "#fab387";
+ #       yellow = "#f9e2af";
+ #       green = "#a6e3a1";
+ #       teal = "#94e2d5";
+ #       sky = "#89dceb";
+ #       sapphire = "#74c7ec";
+ #       blue = "#89b4fa";
+ #       lavender = "#b4befe";
+ #       text = "#cdd6f4";
+ #       subtext1 = "#bac2de";
+ #       subtext0 = "#a6adc8";
+ #       overlay2 = "#9399b2";
+ #       overlay1 = "#7f849c";
+ #       overlay0 = "#6c7086";
+ #       surface2 = "#585b70";
+ #       surface1 = "#45475a";
+ #       surface0 = "#313244";
+ #       base = "#1e1e2e";
+ #       mantle = "#181825";
+ #       crust = "#11111b";
+ #     };
+ #     palettes.catppuccin_latte = {
+ #       rosewater = "#dc8a78";
+ #       flamingo = "#dd7878";
+ #       pink = "#ea76cb";
+ #       mauve = "#8839ef";
+ #       red = "#d20f39";
+ #       maroon = "#e64553";
+ #       peach = "#fe640b";
+ #       yellow = "#df8e1d";
+ #       green = "#40a02b";
+ #       teal = "#179299";
+ #       sky = "#04a5e5";
+ #       sapphire = "#209fb5";
+ #       blue = "#1e66f5";
+ #       lavender = "#7287fd";
+ #       text = "#4c4f69";
+ #       subtext1 = "#5c5f77";
+ #       subtext0 = "#6c6f85";
+ #       overlay2 = "#7c7f93";
+ #       overlay1 = "#8c8fa1";
+ #       overlay0 = "#9ca0b0";
+ #       surface2 = "#acb0be";
+ #       surface1 = "#bcc0cc";
+ #       surface0 = "#ccd0da";
+ #       base = "#eff1f5";
+ #       mantle = "#e6e9ef";
+ #       crust = "#dce0e8";
+ #     };
+ #   };
+ # };
 
 }
 
