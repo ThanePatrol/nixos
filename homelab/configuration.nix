4182fe25d83c297fe4812ed23faf9ac963f24018
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-03-22 20:35:05 +1100

    chore: merge conflict

--- homelab/configuration.nix
+++ homelab/configuration.nix
@@ -6,51 +6,12 @@
 
 let user = "hugh";
 in {
-  imports = [ # Include the results of the hardware scan.
+  imports = [ 
     ./hardware-configuration.nix
-    # config to turn the machine into a router
     #./router.nix # TODO - figure out why this doesn't work
+
+    ./content/content-aggregation.nix
   ];
-  
-  ##########=======CONTENT AGGREGATION==========##########
-  services.sonarr = {
-    enable = true;
-    openFirewall = true;
-  };
-
-  services.radarr = {
-    enable = true;
-    openFirewall = true;
-  };
-
-  #https://github.com/nzbget/nzbget/blob/master/nzbget.conf
-  #services.nzbget = {
-  #  enable = true;
-  #  settings = {
-  #    MainDir = "/nfs/samsung4tb/Content/Downloads";
-  #  };
-  #};
-
-  services.sabnzbd = { enable = true; };
-
-  services.transmission = {
-    enable = true;
-    openFirewall = true;
-    openRPCPort = true;
-    downloadDirPermissions = "757";
-    settings = {
-      download-dir = "/nfs/samsung4tb/Content/Transmission";
-      incomplete-dir = "/nfs/samsung4tb/Content/Transmission";
-      rpc-bind-address = "127.0.0.1,10.0.0.*";
-      rpc-whitelist = "127.0.0.1,10.0.0.*";
-      speed-limit-up-enabled = true;
-      speed-limit-up = 300; # KBs per second
-      speed-limit-down-enabled = true;
-      speed-limit-down = 2000;
-    };
-    webHome = pkgs.flood-for-transmission;
-  };
-  ############################################################
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
