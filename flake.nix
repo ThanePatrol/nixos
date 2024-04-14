fd7b24f537008f40757df65e903684a97712c9ab
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2024-04-14 19:27:56 +1000

    fix: args

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
       androidSystem = username: isWork: email: gitUserName: 
         nix-on-droid.lib.nixOnDroidConfiguration {
           #inherit system pkgs;
-          specialArgs = {
+          extraSpecialArgs = {
             customArgs = {
               inherit isWork email gitUserName;
             };
