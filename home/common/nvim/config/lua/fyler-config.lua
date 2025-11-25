f5804002ff05f91fab27ac076ba2d4ff05733a14
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2025-11-25 21:38:26 +1100

    fyler tree view

--- home/common/nvim/config/lua/fyler-config.lua
+++ home/common/nvim/config/lua/fyler-config.lua
@@ -3,7 +3,7 @@ require("fyler").setup({integrations = {icon = "nvim_web_devicons"}})
 
 M.open_at_cwd = function()
     local cwd = vim.fn.expand("%:h")
-    require('fyler').toggle({dir = cwd})
+    require('fyler').toggle({dir = cwd, kind = 'split_left'})
 end
 
 return M
