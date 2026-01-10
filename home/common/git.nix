3f7f757dde353a478f419a21c0e8e323ef4f80ec
hmandalidis <hmandalidis@google.com>
2026-01-10 16:37:51 +1100

    add githook to config

--- home/common/git.nix
+++ home/common/git.nix
@@ -40,6 +40,7 @@ in
       init.defaultBranch = DEFAULT_BRANCH;
       branch.sort = "-committerdate";
       core.editor = "vim";
+      core.hooksPath = ".githooks";
       http.postBuffer = 524288000; # increase size for larger files
       pull.ff = "only";
       push.autoSetupRemote = true;
