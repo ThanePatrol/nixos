7ed81763cbfe9d8c68d55cc8646ec8190c35ee8a
hmandalidis <hmandalidis@google.com>
2026-03-27 12:28:27 +1100

    code companion use gemini_cli with model 3.1 as default

--- home/common/nvim/config/lua/codecompanion-config.lua
+++ home/common/nvim/config/lua/codecompanion-config.lua
@@ -1,18 +1,23 @@
 require("codecompanion").setup({
+	-- New default from https://github.com/olimorris/codecompanion.nvim/discussions/2643
+    interactions = {chat = {adapter = "gemini_cli", model = "auto-gemini-3"}},
     strategies = {
         chat = {
-            adapter = "gemini",
+            adapter = "gemini_cli",
             variables = {["buffer"] = {opts = {default_params = 'sync_all'}}}
         },
-        inline = {adapter = "gemini"},
-        cmd = {adapter = "gemini"}
+        inline = {adapter = "gemini_cli"},
+        cmd = {adapter = "gemini_cli"}
     },
     display = {chat = {window = {position = 'right'}}},
     adapters = {
         acp = {
             gemini_cli = function()
                 return require("codecompanion.adapters").extend("gemini_cli", {
-                    defaults = {auth_method = "gemini-api-key"}
+                    defaults = {
+                        auth_method = "gemini-api-key",
+                        model = "auto-gemini-3"
+                    }
                 })
             end
         }
