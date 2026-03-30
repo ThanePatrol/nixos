aa3c1e5ae820d8c835b568bc7f8b8eb814d3dc7f
hmandalidis <hmandalidis@google.com>
2026-03-31 10:54:35 +1100

    conf files highlight with txtproto

--- home/common/nvim/config/lua/treesitter-config.lua
+++ home/common/nvim/config/lua/treesitter-config.lua
@@ -5,10 +5,14 @@ vim.api.nvim_create_autocmd("FileType", {
         local lang = vim.treesitter.language.get_lang(ft)
         -- Ignore languages that treesitter fails on
         if lang == 'TelescopeResults' or lang == 'TelescopePrompt' or lang ==
-            'cmp_menu' or lang == 'cmp_docs' or lang == 'fyler' or lang == 'jjdescription' then return end
-        require('nvim-treesitter').install(lang)
-        vim.treesitter.language.add(lang)
-        vim.treesitter.start(args.buf, lang)
+            'cmp_menu' or lang == 'cmp_docs' or lang == 'fyler' or lang ==
+            'jjdescription' or lang == 'conf' then return end
+
+        local lang_to_install = lang
+        if lang == 'conf' then lang_to_install = 'pbtxt' end
+        require('nvim-treesitter').install(lang_to_install)
+        vim.treesitter.language.add(lang_to_install)
+        vim.treesitter.start(args.buf, lang_to_install)
         vim.bo[args.buf].indentexpr =
             "v:lua.require'nvim-treesitter'.indentexpr()"
     end
