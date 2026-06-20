a683a273c1576fe06e9135de3926ec5d3aa6dd5c
Hugh Mandalidis <mandalidis.hugh@gmail.com>
2026-06-20 22:43:55 +1000

    remove redundant rclone conf

--- home/common/rclone.nix
+++ home/common/rclone.nix
@@ -1,14 +1,6 @@
-{ pkgs, osConfig, ... }:
+{ osConfig, ... }:
 
 {
-  home.packages = [ pkgs.rclone ];
-  # home.file."./.config/rclone/rclone.conf".text = ''
-  #   [b2-ironwolfs]
-  #   type = b2
-  #   account = ${osConfig.sops.secrets.backblaze_key_id.path}
-  #   key = ${osConfig.sops.secrets.backblaze_application_key.path}
-  #   hard_delete = true
-  # '';
   programs.rclone = {
     enable = true;
     remotes = {
